248d02c8c8523873f80b6dddeeb8faab20e287c2
Charles-Xzavier <charlesryan929@gmail.com>
2024-06-29 20:29:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let btns = document.querySelectorAll('.toggle-btn');
+let answers = document.querySelectorAll('.answer');
+
+btns.forEach((btn, index) => {
+  btn.addEventListener('click', function () {
+    //let answer = answers[index];
+
+    //to close answers
+    answers.forEach((answer, i) => {
+      if (i !== index) {
+        answer.style.display = 'none';
+        btns[i].src = './assets/images/icon-plus.svg';
+      }
+    });
+
+    //to toggle current answer
+    if (answer.style.display === 'block') {
+      answer.style.display = 'none';
+      btn.src = './assets/images/icon-plus.svg';
+    } else {
+      answer.style.display = 'block';
+      btn.src = './assets/images/icon-minus.svg';
+    }
+  });
+});
